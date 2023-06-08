e298db9112272c475eb3bfecdae30c6f57807f66
Tugolbay Kasymbekov <kasymbekovv1221@gmail.com>
2023-06-08 16:32:36 +0600

    add hm-26

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+import { OrderBasket } from "./OrderBasket";
+
+export const Header = ({ toggleHandler }) => {
+  return (
+    <HeaderStyle>
+      <Container>
+        <MealsText>React Meals</MealsText>
+        <OrderBasket toggleHandler={toggleHandler}>Your Cart</OrderBasket>
+      </Container>
+    </HeaderStyle>
+  );
+};
+
+const HeaderStyle = styled.header`
+  position: fixed;
+  width: 100%;
+  height: 101px;
+  background-color: #8a2b06;
+  padding: 22px 120px;
+  color: #ffffff;
+  top: 0;
+  z-index: 998;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const MealsText = styled.p`
+  font-family: "Poppins";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 38px;
+  line-height: 57px;
+  color: #ffffff;
+`;
